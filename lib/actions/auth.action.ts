8825e6b37c8eb4d32d5993715b051bb326ce8d75
Yuval <yuvalkat@gmail.com>
2025-12-09 16:20:06 +0200

    impliment auth and storage

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -2,7 +2,7 @@
 
 import { db, auth } from "@/firebase/admin";
 import { cookies } from "next/headers";
-import { any } from "zod";
+import { any, promise } from "zod";
 const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export async function signUp(params: SignUpParams) {
@@ -72,4 +72,34 @@ export async function setSessionCookie(idToken: string) {
     path: "/",
     sameSite: "lax",
   });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (e) {
+    console.log(e);
+
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user;
 }
